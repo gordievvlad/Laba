f6509b4eef86b3a7c77b18eeb6a43476921915d0
Sviridy <john591998@mail.ru>
2021-08-30 17:10:12 +0300

    update

--- department/department_app/models/employee.py
+++ department/department_app/models/employee.py
@@ -1,6 +1,9 @@
 """Employee"""
 from django.db import models
-from department_app.models import Department, Positions, Specialization
+# from department_app.models import Department, Positions, Specialization
+from .departmentt import Department
+from .specialization import Specialization
+from .positions import Positions
 
 
 class Employee(models.Model):
