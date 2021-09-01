fb0696afd80ea61eacabff0b785285eadc8d2612
Sviridy <john591998@mail.ru>
2021-09-01 10:18:25 +0300

    Update departmentt.py

--- department/department_app/models/departmentt.py
+++ department/department_app/models/departmentt.py
@@ -6,20 +6,20 @@ class Department(models.Model):
     """Department"""
     department = models.CharField(max_length=25)
 
-    @property
-    def number_of_employees(self):
-        """Number of employees"""
-        return self.employee_set.all().count()
-
-    @property
-    def the_average_salary(self):
-        """The average salary"""
-        wages = 0
-        emp = self.employee_set.all()
-        for i in emp:
-            wages += i.wages
-        summ = wages // self.employee_set.all().count()
-        return summ
+    # @property
+    # def number_of_employees(self):
+    #     """Number of employees"""
+    #     return self.employee_set.all().count()
+    #
+    # @property
+    # def the_average_salary(self):
+    #     """The average salary"""
+    #     wages = 0
+    #     emp = self.employee_set.all()
+    #     for i in emp:
+    #         wages += i.wages
+    #     summ = wages // self.employee_set.all().count()
+    #     return summ
 
     class Meta:
         """Sorting"""
